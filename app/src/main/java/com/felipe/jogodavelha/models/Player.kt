3589338a15cd0624dce26d6cc21ef94603d1c469
Logon Aluno <logonaluno@local.com>
2020-02-03 21:07:32 -0300

    Initial commit Aula Kotlin

---
+++ app/src/main/java/com/felipe/jogodavelha/models/Player.kt
@@ -0,0 +1,3 @@
+package com.felipe.jogodavelha.models
+
+data class Player(var name: String, var value: String)
---
+++ app/src/main/java/com/felipe/jogodavelha/views/game/GameActivity.kt
@@ -0,0 +1,21 @@
+package com.felipe.jogodavelha.views.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.felipe.jogodavelha.R
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+    fun onPlayersSet(player1: String, player2: String) {
+
+    }
+
+    fun promptForPlayers() {
+
+    }
+}
